a496067ed99806e1d91b86e4124f717512ff102d
shreedhar19oct <shreedhar19oct@gmail.com>
2023-01-23 01:50:21 +0530

    frontend

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello,Welcome to my Blog</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Lorem Est dolore et eiusmod ullamco. Occaecat commodo
+         reprehenderit eu tempor adipisicing sint id non ut ea. 
+         Amet sit ipsum et reprehenderit irure laboris officia est
+          cillum ex dolore ipsum. Exercitation magna laboris 
+          cupidatat ad esse irure veniam voluptate voluptate Lorem 
+          excepteur dolore. Magna dolor esse aute adipisicing duis 
+          voluptate exercitation. </p>
+          <p className='mx-auto leading-relaxed text-base mb-4'>Lorem Est dolore et eiusmod ullamco. Occaecat commodo
+         reprehenderit eu tempor adipisicing sint id non ut ea. 
+         Amet sit ipsum et reprehenderit irure laboris officia est
+          cillum ex dolore ipsum. Exercitation magna laboris 
+          cupidatat ad esse irure veniam voluptate voluptate Lorem 
+          excepteur dolore. Magna dolor esse aute adipisicing duis 
+          voluptate exercitation. </p>
+          <p className='mx-auto leading-relaxed text-base mb-4'> Nisi nostrud aute aliquip dolor elit dolor cupidatat eiusmod nisi culpa consequat velit excepteur. Sit adipisicing enim sunt sint deserunt ex incididunt id nulla proident pariatur irure voluptate velit. Et ipsum exercitation elit tempor pariatur aliquip id anim laborum. Elit voluptate sit magna eiusmod sint veniam. Reprehenderit laborum nostrud reprehenderit sunt incididunt sit cillum voluptate.
+
+Anim qui ea ut anim consectetur laborum labore do proident quis occaecat ullamco deserunt magna. Ea esse nisi anim ex consequat nostrud dolor occaecat reprehenderit. Culpa ex officia commodo veniam consectetur incididunt in ipsum minim elit esse ad. Aliquip incididunt culpa do fugiat velit. Sunt sunt mollit adipisicing quis est in est ea ea eu minim. Eiusmod dolore anim laborum occaecat velit dolore quis ea aliquip labore ullamco elit pariatur aute.
+
+Duis ea labore consequat quis dolor voluptate ullamco. Ipsum enim tempor incididunt aliqua proident quis excepteur nostrud mollit officia ad. Non aliqua ipsum laborum tempor nostrud non anim.</p>
+
+    </div>
+  )
+}
+
+export default Home
