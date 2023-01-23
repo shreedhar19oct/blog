cfc62603ae5531693fee3ff1d9254d69d679f236
shreedhar19oct <shreedhar19oct@gmail.com>
2023-01-23 20:31:45 +0530

    frontenddone

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About Us />} />
           <Route path="/articles-list" element={<ArticlesList />} />
-          <Route path="/article" element={<Article />} />
+          <Route path="/article/:name" element={<Article />} />
         </Routes>
       </div>
     </Router>
